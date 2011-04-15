6fe5cfbf4ad73da2402bfe011751cb23aaf914f8
Alex Koppel <alex+git@alexkoppel.com>
2011-04-15 10:34:51 +0200

    Added Jasmine for Javascript testing.

---
+++ spec/javascripts/google_auth_spec.js
@@ -0,0 +1,14 @@
+describe("GoogleAuth", function() {
+  /* page fixture */
+  loadFixtures('google_auth_fixture.html');
+
+  it("should exist", function() {
+    expect(PK).toBeDefined();
+    expect(PK.GoogleAuth).toBeDefined();
+  })
+  
+  describe("initialization", function() {
+    
+  })
+  
+})
