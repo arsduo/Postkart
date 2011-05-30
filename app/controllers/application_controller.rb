14e7669c2b9697fa8217def1abd299483c66b713
Alex Koppel <alex+git@alexkoppel.com>
2011-05-30 13:52:31 +0200

    The reloadData flag also now is redirected away when used.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,20 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-
+  
+  before_filter :setup_dev if Rails.env.development?
+  before_filter :detect_contact_reload
+  
   include MobileControllerExtensions
 
   private
+
+  def detect_contact_reload
+    if params.delete(:reloadData)
+      # set the reload flag, then get rid of the extra URL flag to avoid confusing jQM
+      flash[:reloadData] = true
+      redirect_to params
+    end
+  end
 
   def ensure_signed_in
     render :json => {:error => {:loginRequired => true}} unless user_signed_in?
@@ -15,6 +26,13 @@ class ApplicationController < ActionController::Base
       options = (request.env['exception_notifier.options'] ||= {})
       options.reverse_merge!(ExceptionNotifierOptions)
       ExceptionNotifier::Notifier.exception_notification(request.env, exception).deliver
+    end
+  end
+  
+  def setup_dev
+    if Rails.env.development? && params[:pow]
+      sign_in(User.first)
+      redirect_to root_path   
     end
   end
 end
--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -1,7 +1,45 @@
 require 'spec_helper'
 
+class VanillaController < ApplicationController
+  include MobileControllerExtensions
+  def act
+    render :text => ""
+  end
+end
+
 describe ApplicationController do
   it "includes MobileControllerExtensions" do
     controller.class.included_modules.should include MobileControllerExtensions
+  end  
+end
+
+describe VanillaController do
+  include RSpec::Rails::ControllerExampleGroup
+  describe "universal before_filters" do
+    before :all do
+      @controller_name = "vanilla"
+      @action_name = "act"
+      Postkart::Application.routes.draw do
+        get "#{@controller_name}/#{@action_name}", :controller => :vanilla, :action => :act
+      end    
+    end
+  
+    after :all do
+      # restore all routes
+      Postkart::Application.reload_routes!
+    end
+  
+    describe ".detect_contact_reload" do
+      it "sets flash[:reloadData] = true if params[:reloadData]" do
+        get @action_name, :reloadData => 1
+        flash[:reloadData].should be_true
+      end
+    
+      it "redirects to the page w/o the mobile flag if it's present" do
+        params = HashWithIndifferentAccess.new(:reloadData => 1, :foo => "bar", :controller => @controller_name, :action => @action_name)
+        controller.expects(:redirect_to).with(params.dup.delete_if {|k, v| k.to_sym == :reloadData})
+        get @action_name, params
+      end
+    end
   end
 end
